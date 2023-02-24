396ae60d4270da8552bcae62a2c63daa69e6d6a4
Shyanne <randomgirlisweird@gmail.com>
2023-02-24 13:01:34 -0500

    Added AboutCommand

---
+++ src/main/java/sh0inx/incentive/commands/AboutCommand.java
@@ -0,0 +1,23 @@
+package sh0inx.incentive.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import sh0inx.incentive.Incentive;
+
+public class AboutCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+
+        sender.sendMessage("""
+                Incentive
+                author: %s
+                version: %s
+                modrinth: %s
+                source: %s""",
+                Incentive.author, Incentive.version, Incentive.link, Incentive.source);
+
+        return false;
+    }
+}
