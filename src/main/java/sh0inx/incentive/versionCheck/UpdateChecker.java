f61b21d6728dd9e1bac764d424eec6452a51abef
Shyanne <randomgirlisweird@gmail.com>
2023-02-25 16:27:18 -0500

    Changed project URL to include project slug

---
+++ src/main/java/sh0inx/incentive/versionCheck/UpdateChecker.java
@@ -0,0 +1,46 @@
+package sh0inx.incentive.versionCheck;
+
+import sh0inx.incentive.Incentive;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+
+public class UpdateChecker {
+
+    public static String getCurrentVersion() {
+        Gson gson = new Gson();
+        StringBuilder response = new StringBuilder();
+
+        try {
+            URL url = new URL("https://api.modrinth.com/v2/project/" + Incentive.pluginID + "/version");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+            System.out.println(Incentive.debugPrefix + "RESPONSE: " + responseCode);
+
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            } else {
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    response.append(scanner.nextLine());
+                }
+                scanner.close();
+            }
+
+            jsonModel[] mapVersion = gson.fromJson(response.toString(), jsonModel[].class);
+            return mapVersion[0].currentVersion;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Unable to check for current version.";
+        }
+    }
+}
