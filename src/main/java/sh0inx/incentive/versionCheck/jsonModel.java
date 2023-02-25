e7d319378c1899c153c38cfe45566e55596491c6
Shyanne <randomgirlisweird@gmail.com>
2023-02-25 16:26:52 -0500

    Changed variable name to be more Java-sih (used Serialized Name as reference)

---
+++ src/main/java/sh0inx/incentive/versionCheck/jsonModel.java
@@ -0,0 +1,7 @@
+package sh0inx.incentive.versionCheck;
+import com.google.gson.annotations.SerializedName;
+
+public class jsonModel {
+    @SerializedName("version_number")
+    String currentVersion;
+}
