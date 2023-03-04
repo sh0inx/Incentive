4ed2f6957e9896d2f17eb324d2ec10fd5a1ed3bb
Shyanne <randomgirlisweird@gmail.com>
2023-03-04 13:03:31 -0500

    Reformatted message and fixed typo.

--- src/main/java/sh0inx/incentive/commands/HelpCommand.java
+++ src/main/java/sh0inx/incentive/commands/HelpCommand.java
@@ -3,24 +3,23 @@ package sh0inx.incentive.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import sh0inx.incentive.Incentive;
 
 public class HelpCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
 
-        //TODO: Implement pages and clickthrough navigation for the help command.
+        //TODO: Implement pages and click-through navigation for the help command.
 
-        String message = (
-                "Incentive" +
-                "\nprefix: inc (incentive, reward, rewards)" +
-                "\n" +
-                "\nabout: shows information about Incentive." +
-                "\nprofile: shows the server profile that Incentive has loaded." +
-                "\nhelp: shows this menu."
-                );
-
-        sender.sendMessage(message);
+        sender.sendMessage(String.format("""
+                        %s
+                        prefix: inc (incentive, reward, rewards)
+           
+                        about: shows information about Incentive.
+                        profile: shows the server profile that Incentive has loaded.
+                        help: shows this menu.
+                        """), Incentive.commandPrefix);
 
         return true;
     }
